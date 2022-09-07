02e7cc10a8c4b371108573d6313b2ffd91dbe584
Rouberth Oliveira Quirino <rouberth@gmail.com>
2022-09-07 17:20:28 -0300

    jogo jokenpo, com a funcionalidade jogar funcionando

---
+++ jokenpo.js
@@ -0,0 +1,48 @@
+/**
+ * Projeto 1 - Jokenpô
+ * 
+ * 
+ */
+function jogar(){
+    if(document.getElementById("pedra").checked == false && document.
+    getElementById("papel").checked == false && document.
+    getElementById("tesoura").checked == false){
+        alert("Selecione uma opção");
+
+    }else{
+        //logica principal
+        var sorteio = Math.floor(Math.random() * 3);
+        switch(sorteio){
+            case 0:
+                document.getElementById("pc").src="img/pcpedra.png";
+                    break;
+            case 1:
+                document.getElementById("pc").src="img/pcpapel.png";
+                    break;
+            case 2:
+                document.getElementById("pc").src="img/pctesoura.png";
+                    break;            
+        }
+        // verificar o vencedor ou empate
+        if((document.getElementById("pedra").checked == true && sorteio == 0) 
+        ||(document.getElementById("papel").checked == true && sorteio == 1) 
+        ||(document.getElementById("tesoura").checked == true && sorteio ==2)){
+            document.getElementById("placar").innerHTML="Empate";
+        } else if ((document.getElementById("pedra").checked == true && sorteio == 2) 
+        ||(document.getElementById("papel").checked == true && sorteio == 0) 
+        ||(document.getElementById("tesoura").checked == true && sorteio == 1)){
+            document.getElementById("placar").innerHTML="Você venceu!";
+        } else{
+            document.getElementById("placar").innerHTML="O PC ganhou!";
+        }
+
+        
+        
+    }
+}
+
+function resetar(){
+    document.getElementById("pc") .src="pc.png";
+    document.getElementById("placar") .innerHTML="";
+
+}
